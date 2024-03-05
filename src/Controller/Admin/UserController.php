d2cae76c6e9e3cabbfb64bd06d052565cf93b612
fagathe-dev <agathefrederick@gmail.com>
2024-03-05 12:03:11 +0100

    front: create user form #35

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\App\Form\Admin\CreateUserType;
+use App\Service\Breadcrumb\BreadcrumbItem;
 use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,11 +17,24 @@ final class UserController extends AbstractController
 
     public function __construct(
         private UserService $service
-    ) {}
+    ) {
+    }
 
     #[Route('', name: 'index', methods: ['GET'])]
     public function index(Request $request): Response
     {
         return $this->render('admin/user/index.html.twig', $this->service->index($request));
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $breadcrumb = $this->service->breadcrumb([
+            new BreadcrumbItem('Nouveau'),
+        ]);
+        $user = new User;
+        $form = $this->createForm(CreateUserType::class, $user);
+
+        return $this->render('admin/user/new.html.twig', compact('breadcrumb', 'user', 'form'));
     }
 }
---
+++ src/Form/Admin/CreateUserType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form\App\Form\Admin;
+
+use App\Entity\User;
+use App\Enum\User\RoleEnum;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'label' => 'Adresse e-mail',
+            ])
+            ->add('username', TextType::class, [
+                'required' => true,
+                'label' => 'Nom d\'utilisateur',
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Rôles',
+                'choices' => RoleEnum::choices(),
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'btn btn-primary'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
