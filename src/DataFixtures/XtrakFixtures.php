8513c46c6791a8f69311268dfc17e51e32b6894d
fagathe-dev <agathefrederick@gmail.com>
2024-03-25 02:08:37 +0100

    data: generate xtrakCode fake data #81

--- src/DataFixtures/XtrakFixtures.php
+++ src/DataFixtures/XtrakFixtures.php
@@ -2,7 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\XtrakCode;
 use App\Entity\XtrakSite;
+use App\Enum\Xtrak\EnvEnum;
+use App\Helpers\DateTimeHelperTrait;
 use App\Utils\FakerTrait;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,6 +14,7 @@ class XtrakFixtures extends Fixture
 {
 
     use FakerTrait;
+    use DateTimeHelperTrait;
 
     public function load(ObjectManager $manager): void
     {
@@ -23,6 +27,20 @@ class XtrakFixtures extends Fixture
                 ->setCreatedAt($this->setDateTimeBetween('-6 months'))
                 ->setUpdatedAt($this->setDateTimeAfter($site->getCreatedAt()))
                 ->setIsActive($this->randomElement([true, false]));
+            $domain = $faker->domainName();
+
+            foreach (EnvEnum::cases() as $env) {
+                $xtrakCode = new XtrakCode;
+
+                $xtrakCode->setDomain(strtolower($env) . '.' . $domain)
+                    ->setEnv($env)
+                    ->setIsActive($xtrakCode->getEnv() !== EnvEnum::ENV_PROD)
+                    ->setCreatedAt($this->setDateTimeAfter($site->getCreatedAt()))
+                    ->setUpdatedAt($this->setDateTimeAfter($xtrakCode->getCreatedAt()))
+                    ->setCode($site->getName() . '_' . $xtrakCode->getEnv());
+
+                $site->addXtrakCode($xtrakCode);
+            }
 
             $manager->persist($site);
         }
